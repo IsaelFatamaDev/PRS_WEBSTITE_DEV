fe7567a83f895d846947b11bfc0bc471238abb6e
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2025-08-03 16:57:20 -0500

    fix(environment): corregir URL de inventoryApiUrl para entorno de producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,5 +9,5 @@ export const environment = {
      street: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/streets',
      qualityTest: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/qualitytests',
      incidents: 'https://lab.vallegrande.edu.pe/jass/ms-claim-incidents/api/v1',
-     inventoryApiUrl: 'http://localhost:8088/api/v1'
+     inventoryApiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-inventory/api/v1/'
 };
