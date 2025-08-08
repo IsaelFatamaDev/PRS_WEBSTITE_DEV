3a93c805e0f4d4ea422e93326ce32d12c67ebf82
CC-VictorCuaresma <118938567+CC-VictorCuaresma@users.noreply.github.com>
2025-08-08 05:22:46 +0000

    Corrección de carpeta/archivos subidos por error

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,18 +1,5 @@
 export const environment = {
      production: true,
-<<<<<<< HEAD
-     enableConsoleLog: false,
-     apiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-user/api/v1',
-     complaintsIncidentsApiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-claim-incidents/api/v1',
-     organizations: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/organizations',
-     testingPoint: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/testingpoints',
-     dailyRecords: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/dailyrecords',
-     zonas: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/zones',
-     street: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/streets',
-     qualityTest: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/qualitytests',
-     incidents: 'https://lab.vallegrande.edu.pe/jass/ms-claim-incidents/api/v1',
-     inventoryApiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-inventory/api/v1/'
-=======
      apiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-user/api/v1',
      organizations: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/organizations',
      distribution: 'https://lab.vallegrande.edu.pe/jass/ms-distribution/api/v2',
@@ -22,5 +9,4 @@ export const environment = {
      street: 'https://lab.vallegrande.edu.pe/jass/ms-organizations/api/streets',
      qualityTest: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/qualitytests',
      users: 'https://lab.vallegrande.edu.pe/jass/ms-user/api/v1/api/v1/users/all'
->>>>>>> d210d18 (Actualización ms-distribucion-agua)
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,5 @@
 export const environment = {
      production: false,
-<<<<<<< HEAD
-     enableConsoleLog: true,
-     apiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-user/api/v1',
-     complaintsIncidentsApiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-claim-incidents/api/v1',
-     organizations: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/organizations',
-     testingPoint: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/testingpoints',
-     dailyRecords: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/dailyrecords',
-     zonas: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/zones',
-     street: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/streets',
-     qualityTest: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/qualitytests',
-     incidents: 'https://lab.vallegrande.edu.pe/jass/ms-claim-incidents/api/v1',
-     inventoryApiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-inventory/api/v1/',
-     infrastructureApiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-infrastructure/api/v1'
-=======
      apiUrl: 'https://lab.vallegrande.edu.pe/jass/ms-user/api/v1',
      complaintsIncidentsApiUrl: 'https://vg-ms-claims-incidents-production.up.railway.app/api/v1',
      organizations: 'https://lab.vallegrande.edu.pe/jass/ms-organization/api/organizations',
@@ -25,5 +11,4 @@ export const environment = {
      qualityTest: 'https://lab.vallegrande.edu.pe/jass/ms-water-quality/api/v2/qualitytests',
      users: 'https://lab.vallegrande.edu.pe/jass/ms-user/api/v1/api/v1/users/all',
      incidents: 'https://vg-ms-claims-production.up.railway.app/api/v1',
->>>>>>> d210d18 (Actualización ms-distribucion-agua)
 };
